75071d902e8278426bf231b5dddd119e8a5340a5
larawr <103736158+larawr@users.noreply.github.com>
2024-09-20 13:26:53 +0800

    Update ITDBADM_OUTPUT01.sql

--- ITDBADM_OUTPUT01.sql
+++ ITDBADM_OUTPUT01.sql
@@ -1,4 +1,9 @@
 -- Create users here
+CREATE USER 'salesmodule'      IDENTIFIED BY 'DLSU1234!';
+CREATE USER 'customersmodule'      IDENTIFIED BY 'DLSU1234!';
+CREATE USER 'paymentsmodule'      IDENTIFIED BY 'DLSU1234!';
+CREATE USER 'employeesmodule'      IDENTIFIED BY 'DLSU1234!';
+CREATE USER 'productsmodule'      IDENTIFIED BY 'DLSU1234!';
 
 
 -- CRUD Matrix

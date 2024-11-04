c2970bee8ffbf7c4363435c7a749e8299123936e
larawr <103736158+larawr@users.noreply.github.com>
2024-11-04 21:41:25 +0800

    added part 4c.a-4c.c

--- Reliable DB Development/05_G107_Creating Triggers to Implement Business Rules.sql
+++ Reliable DB Development/05_G107_Creating Triggers to Implement Business Rules.sql
@@ -314,6 +314,16 @@ BEGIN
 END$$
 DELIMITER ;
 
+-- PART 4C.C (TAN)
+-- create a column in the employees table that signifies whether an employee is active or not
+ALTER TABLE `dbsalesv2.0`.`employees` 
+ADD COLUMN `activeRecord` ENUM('Y', 'N') NULL AFTER `employee_type`;
+
+-- set a value for the new column for those rows who don't have any value in that column
+UPDATE `dbsalesv2.0`.employees
+SET activeRecord = 'Y'
+WHERE activeRecord IS NULL;
+
 -- create trigger that will stop employee names and number from being updated
 DROP TRIGGER IF EXISTS employees_BEFORE_UPDATE;
 DELIMITER $$

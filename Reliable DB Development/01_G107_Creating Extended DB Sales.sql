f6986ef3d5200bbcc5ae4a1024eb22557e9c9dcf
Jsf <josef_michael_tan@dlsu.edu.ph>
2024-11-05 12:27:19 +0800

    moved part 4c-a
    moved part 4c-a from extended db creation to triggers script so it won't interfere with migration

--- Reliable DB Development/01_G107_Creating Extended DB Sales.sql
+++ Reliable DB Development/01_G107_Creating Extended DB Sales.sql
@@ -55,34 +55,6 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
--- PART 4C-A (TAN)
--- alter the employee_type column so that old values can be edited
-ALTER TABLE `dbsalesv2.0`.employees
-MODIFY employee_type VARCHAR(50);
-
--- replace old values with ones in a new format
-UPDATE `dbsalesv2.0`.employees
-SET employee_type = 'Sales Representative'
-WHERE employee_type = 'S';
-
-UPDATE `dbsalesv2.0`.employees
-SET employee_type = 'Sales Manager'
-WHERE employee_type = 'N';
-
--- change the employee_type column to 'not null' and change the values accepted by changing the type to ENUM
-ALTER TABLE `dbsalesv2.0`.`employees` 
-CHANGE COLUMN `employee_type` `employee_type` ENUM('Sales Representative', 'Sales Manager', 'Inventory Manager') NOT NULL ;
-
--- fix typographical errors in existing database
-UPDATE `dbsalesv2.0`.employees
-SET jobTitle = 'Sales Manager (EMEA)'
-WHERE jobTitle = 'Sale Manager (EMEA)';
-
--- alter employees table to only allow job titles that exist in the organization (as seen in the existing database)
-ALTER TABLE `dbsalesv2.0`.`employees` 
-CHANGE COLUMN `jobTitle` `jobTitle` ENUM('President', 'VP Sales', 'VP Marketing', 'Sales Manager (APAC)', 'Sales Manager (EMEA)', 'Sales Manager (NA)', 'Sales Manager', 'Sales Rep') NOT NULL ;
-
-
 -- -----------------------------------------------------
 -- Table `dbsalesV2.0`.`Non_SalesRepresentatives`
 -- -----------------------------------------------------

8595479c44291f542e59885e68d12be906e9b03d
larawr <103736158+larawr@users.noreply.github.com>
2024-11-05 15:24:39 +0800

    Update PART4D.sql

--- Reliable DB Development/Standalone scripts/PART4D.sql
+++ Reliable DB Development/Standalone scripts/PART4D.sql
@@ -6,7 +6,7 @@ ON SCHEDULE EVERY 1 MONTH
 STARTS '2024-11-01 00:00:00'
 DO
 BEGIN
-    -- Update credit limits based on total order amount for each customer in December 2003
+    -- Update credit limits based on total order amount for each customer in current month
     UPDATE customers c
     JOIN (
         SELECT 
@@ -17,15 +17,15 @@ BEGIN
         JOIN 
             orderdetails od ON o.orderNumber = od.orderNumber
         WHERE 
-            MONTH(o.orderDate) = 12  -- December
-            AND YEAR(o.orderDate) = 2003  -- 2003
+            MONTH(o.orderDate) = MONTH(CURDATE())
+            AND YEAR(o.orderDate) = YEAR(CURDATE())
         GROUP BY 
             o.customerNumber
     ) AS customerOrders ON c.customerNumber = customerOrders.customerNumber
     SET 
         c.creditLimit = customerOrders.customerTotalAmount * 2;
 
-    -- Additional credit for customers with more than 15 distinct orders in December 2003
+    -- Additional credit for customers with more than 15 distinct orders in current month
     UPDATE customers c
     JOIN (
         SELECT 
@@ -36,8 +36,8 @@ BEGIN
         JOIN 
             orderdetails od ON o.orderNumber = od.orderNumber
         WHERE 
-            MONTH(o.orderDate) = 12
-            AND YEAR(o.orderDate) = 2003
+            MONTH(o.orderDate) = MONTH(CURDATE())
+            AND YEAR(o.orderDate) = YEAR(CURDATE())
         GROUP BY 
             o.customerNumber
         HAVING 

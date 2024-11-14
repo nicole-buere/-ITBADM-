6f977391d49af09e4c1f8f193eb2059b3838d7a3
Jsf <josef_michael_tan@dlsu.edu.ph>
2024-11-14 21:40:45 +0800

    test script changes
    for testing, more commands were added to be used for test purposes

--- Standalone scripts/Test script.sql
+++ Standalone scripts/Test script.sql
@@ -17,6 +17,33 @@ WHERE `orderNumber` = '10260' AND status = 'Cancelled';
 UPDATE `dbsalesv2.0`.`orders` SET `status` = 'Cancelled' 
 WHERE (`orderNumber` = '10101');
 
+-- 4B.E test script (TAN)
+
+-- select
+SELECT * FROM `dbsalesv2.0`.discontinued_products;
+
+SELECT * FROM `dbsalesv2.0`.current_products;
+
+SELECT * FROM `dbsalesv2.0`.products;
+
+SELECT * FROM `dbsalesv2.0`.inventory_managers;
+
+-- insert an inventory manager (should pass)
+INSERT INTO `dbsalesv2.0`.`inventory_managers` (`employeeNumber`) 
+VALUES ('1002');
+
+-- discontinue a product without specifying an inventory manager (should fail)
+UPDATE `dbsalesv2.0`.`current_products` SET `current_status` = 'D' 
+WHERE (`productCode` = 'S10_1678');
+
+-- discontinue a product while specifying an inventory manager (should succeed)
+UPDATE `dbsalesv2.0`.`current_products` SET `current_status` = 'D', `discontinuing_manager` = '1002', `discontinue_reason` = 'Not up to standard'
+WHERE (`productCode` = 'S10_1678');
+
+-- re-continue a product (should succeed)
+UPDATE `dbsalesv2.0`.`current_products` SET `current_status` = 'C' 
+WHERE (`productCode` = 'S10_1678');
+
 -- 4C.A test script (TAN)
 
 -- insert an employee record without specifying job title or employee type (should fail)
@@ -30,6 +57,9 @@ VALUES ('9999', 'Test', 'Man', 'x1234', 'testmand@classicmodelcars.com', 'faketi
 -- insert an employee record with a valid job title and employee type (should pass)
 INSERT INTO `dbsalesv2.0`.`employees` (`employeeNumber`, `lastName`, `firstName`, `extension`, `email`, `jobTitle`, `employee_type`, `activeRecord`) 
 VALUES ('9999', 'Test', 'Man', 'x1234', 'testmand@classicmodelcars.com', 'Sales Rep', 'Sales Representative', 'Y');
+
+-- change an employee type to Sales Manager (should pass)
+UPDATE `dbsalesv2.0`.`employees` SET `employee_type` = 'Sales Manager' WHERE (`employeeNumber` = '9999');
 
 -- select all employees
 SELECT * FROM `dbsalesv2.0`.employees;
@@ -57,33 +87,6 @@ WHERE (`employeeNumber` = '9999');
 -- delete an employee record (should fail)
 DELETE FROM `dbsalesv2.0`.`employees` 
 WHERE (`employeeNumber` = '9999');
-
--- 4B.E test script (TAN)
-
--- select
-SELECT * FROM `dbsalesv2.0`.discontinued_products;
-
-SELECT * FROM `dbsalesv2.0`.current_products;
-
-SELECT * FROM `dbsalesv2.0`.products;
-
-SELECT * FROM `dbsalesv2.0`.inventory_managers;
-
--- insert an inventory manager
-INSERT INTO `dbsalesv2.0`.`inventory_managers` (`employeeNumber`) 
-VALUES ('1002');
-
--- discontinue a product without specifying an inventory manager (should fail)
-UPDATE `dbsalesv2.0`.`current_products` SET `current_status` = 'D' 
-WHERE (`productCode` = 'S10_1678');
-
--- discontinue a product while specifying an inventory manager (should succeed)
-UPDATE `dbsalesv2.0`.`current_products` SET `current_status` = 'D', `discontinuing_manager` = '1002'
-WHERE (`productCode` = 'S10_1678');
-
--- re-continue a product (should succeed)
-UPDATE `dbsalesv2.0`.`current_products` SET `current_status` = 'C' 
-WHERE (`productCode` = 'S10_1678');
 
 -- Audit table testing
 
